8e98f3a40896f0df5e11340e2a86d040510183b5
Pear256 <wavecube05@gmail.com>
2025-12-04 13:34:59 -0600

    Update ab.js

--- assets/js/ab.js
+++ assets/js/ab.js
@@ -30,4 +30,5 @@ function openGame(url) {
   icon.href = "https://myfreedrawings.com/wp-content/uploads/2022/07/Fresh-Green-Pear-Clipart-PNG.png";
   icon.rel = "icon";
   icon.type = "icon/png";
+  win.document.head.appendChild(icon)
 }
